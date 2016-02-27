d0c89e0c42fb94ceaef78de21163130b3cf2e2e5
Cari Cesarotti <cjc359@cornell.edu>
2016-02-27 01:25:21 -0500

    running with madgraph events normally

--- bhabha50k/aluminNoMag/runbgd.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash                                                                                       
-#parameters you need to specify before runing the script                                           
-source /opt/rh/devtoolset-3/enable
-source /nfs/opt/root/bin/thisroot.sh
-
-g++ `root-config --cflags --libs` -o run scdbgd.C
-./run
